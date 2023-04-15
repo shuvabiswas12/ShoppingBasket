6ede78749d0fbb447cd89e2409ae9836cf28a257
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-04-15 22:19:02 +0600

    Added A class library named "CommonHelper" and Added website roles class.

---
+++ ShoppingBasket.CommonHelper/WebsiteRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket.CommonHelper
+{
+    internal class WebsiteRoles
+    {
+        public static String UserRole = "User";
+        public static String AdminRole = "Admin";
+        public static String EmployeeRole = "Employee";
+    }
+}
