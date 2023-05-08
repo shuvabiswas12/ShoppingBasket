97afbb6885b0b3aad04b9d7c2072c0fb96191bd7
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-05-08 20:48:32 +0600

    Added dbSet<> for Category and Product table.

--- ShoppingBasket.DataAccessLayer/ApplicationDbContext.cs
+++ ShoppingBasket.DataAccessLayer/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ShoppingBasket.Models;
 
 namespace ShoppingBasket.DataAccessLayer;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
     }
