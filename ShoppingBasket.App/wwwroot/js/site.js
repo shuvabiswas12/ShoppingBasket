04228ebb139ca858e72cb455b9c7b092c586a5c1
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-05-22 19:52:42 +0600

    404 page added. some css correction has been done for header and footer viewing on 404 pages.

--- ShoppingBasket.App/wwwroot/js/site.js
+++ ShoppingBasket.App/wwwroot/js/site.js
@@ -1,6 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
+﻿// for main tag's height for all pages
 document.getElementsByTagName('main')[0].style.minHeight = (window.screen.height - 400)+ "px";
+
+
+// hide footer tag when 404 page is active
+if (document.getElementById("notFound")) {
+    document.getElementById("footer").style.display = "none";
+    document.getElementById("header").style.display = "none";
+    document.getElementById("Humberger").style.display = "none";
+}
