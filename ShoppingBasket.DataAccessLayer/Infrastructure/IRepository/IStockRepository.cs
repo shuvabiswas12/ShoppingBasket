fb59f8c2200a6c82b6a935818c5c3cb082acfbf4
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-05-24 00:36:46 +0600

    some bug has been fixed.

--- ShoppingBasket.DataAccessLayer/Infrastructure/IRepository/IStockRepository.cs
+++ ShoppingBasket.DataAccessLayer/Infrastructure/IRepository/IStockRepository.cs
@@ -2,7 +2,7 @@ using ShoppingBasket.Models;
 
 namespace ShoppingBasket.DataAccessLayer.Infrastructure.IRepository;
 
-public interface IStockRepository
+public interface IStockRepository : IRepository<Stock>
 {
     void Update(Stock stock);
 }
