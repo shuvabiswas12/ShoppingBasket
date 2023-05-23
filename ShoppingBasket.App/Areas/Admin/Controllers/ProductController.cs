2365a6b2a2122097c3df609eb0cc8e40875e15ad
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-05-23 13:29:56 +0600

    Add Product form modified.

--- ShoppingBasket.App/Areas/Admin/Controllers/ProductController.cs
+++ ShoppingBasket.App/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShoppingBasket.DataAccessLayer.Infrastructure.IRepository;
+using ShoppingBasket.Models;
 using ShoppingBasket.Models.ViewModels;
 
 namespace ShoppingBasket.App.Areas.Admin.Controllers;
@@ -6,6 +9,13 @@ namespace ShoppingBasket.App.Areas.Admin.Controllers;
 [Area("Admin")]
 public class ProductController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
     // GET
     public IActionResult Index()
     {
@@ -13,9 +23,29 @@ public class ProductController : Controller
     }
 
     // GET
-    public IActionResult Create()
+    public IActionResult CreateAndUpdate(int? id)
     {
-        ProductVm productVm = new ProductVm();
+        ProductVm productVm = new ProductVm()
+        {
+            Product = new Product(),
+            Categories = _unitOfWork.CategoryRepository.GetAll().Select(c => new SelectListItem()
+            {
+                Text = c.Name,
+                Value = c.Id.ToString()
+            }),
+            Stock = new Stock()
+        };
         return View(productVm);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CreateAndUpdate(ProductVm productVm, IFormFile ? file)
+    {
+        if (ModelState.IsValid)
+        {
+        }
+
+        return View();
     }
 }
--- ShoppingBasket.Models/ViewModels/ProductVM.cs
+++ ShoppingBasket.Models/ViewModels/ProductVM.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace ShoppingBasket.Models.ViewModels;
 
 public class ProductVm
 {
     public Product Product { get; set; }
-    public Stock Stock { get; set; }
+
+    [ValidateNever] public IEnumerable<SelectListItem> Categories { get; set; }
+    [ValidateNever] public Stock Stock { get; set; }
 }
