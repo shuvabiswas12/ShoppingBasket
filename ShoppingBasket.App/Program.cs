335b0e75362d3ff8a66254ea2b19f6b8f1e595f4
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-05-25 11:47:44 +0600

    fixed a issue for circular cycle in JsonSerializerOptions.

--- ShoppingBasket.App/Program.cs
+++ ShoppingBasket.App/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using ShoppingBasket.DataAccessLayer;
 using ShoppingBasket.DataAccessLayer.Infrastructure.IRepository;
@@ -7,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// For ignoring circular references cycle error
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
