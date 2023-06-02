f1877d7eb2d2cd5b344f755c2335d3977b472583
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-06-02 14:34:24 +0600

    Payment status and order status are added.

---
+++ ShoppingBasket.CommonHelper/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket.CommonHelper
+{
+    public class OrderStatus
+    {
+        public const string STATUS_PENDING = "Pending";
+        public const string STATUS_APPROVED = "Approved";
+        public const string STATUS_CANCELLED = "Cancelled";
+        public const string STATUS_REFUNDED = "Refunded";
+        public const string STATUS_PROCESSING = "Processing";
+        public const string STATUS_SHIPPED = "Shipped";
+    }
+}
---
+++ ShoppingBasket.CommonHelper/PaymentStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket.CommonHelper
+{
+    public class PaymentStatus
+    {
+        public const string STATUS_PENDING = "Pending";
+        public const string STATUS_APPROVED = "Approved";
+        public const string STATUS_REJECTED = "Rejected";
+    }
+}
