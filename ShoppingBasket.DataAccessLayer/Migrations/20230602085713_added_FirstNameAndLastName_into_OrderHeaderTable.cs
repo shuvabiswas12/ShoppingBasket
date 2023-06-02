c7370c0feb141b4eb8ea69b1b42507a312ddbcdd
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-06-02 14:58:52 +0600

    Migrations for newly created two new columns on order header table.

---
+++ ShoppingBasket.DataAccessLayer/Migrations/20230602085713_added_FirstNameAndLastName_into_OrderHeaderTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingBasket.DataAccessLayer.Migrations
+{
+    public partial class added_FirstNameAndLastName_into_OrderHeaderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "OrderHeaders",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "OrderHeaders");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "OrderHeaders",
+                newName: "Name");
+        }
+    }
+}
