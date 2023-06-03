41c1ada6d21a55433f68ec31e1f4aa3701833547
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-06-03 12:11:06 +0600

    added paymentTypes.

---
+++ ShoppingBasket.CommonHelper/PaymentTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket.CommonHelper
+{
+    public static class PaymentTypes
+    {
+        public const string CashOnDelivery = "CASH_ON_DELIVERY";
+        public const string PaymentOnline = "PAYMENT_ONLINE";
+    }
+}
