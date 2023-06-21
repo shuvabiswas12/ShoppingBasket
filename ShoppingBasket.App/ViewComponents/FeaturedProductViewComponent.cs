768cd11158281e82d8135bd35697fcae5fe3ed4b
shuvabiswas12 <shuvabiswas12@gmail.com>
2023-06-21 21:55:53 +0600

    Added featuredProduct view component and some ui changes done.

---
+++ ShoppingBasket.App/ViewComponents/FeaturedProductViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingBasket.DataAccessLayer.Infrastructure.IRepository;
+
+namespace ShoppingBasket.App.ViewComponents
+{
+    public class FeaturedProductViewComponent : ViewComponent
+    {
+        private readonly ILogger<FeaturedProductViewComponent> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FeaturedProductViewComponent(ILogger<FeaturedProductViewComponent> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var featuredProducts = _unitOfWork.ProductRepository.GetAll(includeProperties: "Stock", predicate: p => p.IsFeatured == true);
+            return View(featuredProducts);
+        }
+    }
+}
